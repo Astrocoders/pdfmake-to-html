7c57d265a06d0584ef60e389f70516e9baa63532
Gabriel Rubens <grsabreu@gmail.com>
2017-05-08 14:46:43 -0300

    Remove unnecessary file

--- tests/login.js
+++
@@ -1,5 +0,0 @@
-const NuBank = require('./index.js').default()
-
-NuBank.getLoginToken({ password: 'YOUR_PASSWORD', login: 'YOUR_CPF_NO_DASHES_OR_DOTS' }).then(() => {
-  NuBank.getWholeFeed().then(r => console.log(r))
-})
