dbf61562718860d1a623baaf4546596302e08aee
Gabriel Rubens <grsabreu@gmail.com>
2017-05-08 10:21:37 -0300

    Upgrade everything

--- dist/index.js
+++ dist/index.js
@@ -6,144 +6,111 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-exports.default = function () {
-  var _desc, _value, _obj, _init, _init2, _init3;
-
-  var signInData = {};
-
-  function withSignedInUser(fn) {
-    return function () {
-      if ((0, _lodash.isEmpty)(signInData)) {
-        throw new Error('[NuBank] You must sign in first');
-      }
-
-      return fn.apply(undefined, arguments);
-    };
-  }
-
-  return _obj = {
-    getLoginToken: function getLoginToken(_ref) {
-      var password = _ref.password;
-      var login = _ref.login;
-      return (0, _nodeFetch2.default)(_api_uris2.default.token, {
-        body: JSON.stringify({
-          password: password,
-          login: login,
-          grant_type: 'password',
-          client_id: 'other.conta',
-          client_secret: 'yQPeLzoHuJzlMMSAjC-LgNUJdUecx8XO'
-        }),
-        method: 'POST',
-        headers: _extends({}, REQUEST_HEADERS_SAUCE)
-      }).then(function (res) {
-        return res.json();
-      }).then(function (data) {
-        return signInData = data;
-      });
-    },
-
-    getCustomer: function getCustomer() {
-      return (0, _nodeFetch2.default)(_api_uris2.default.customers, {
-        headers: _extends({}, REQUEST_HEADERS_SAUCE, {
-          Authorization: 'Bearer ' + signInData.access_token
-        })
-      }).then(function (res) {
-        return res.json();
-      });
-    },
-
-    getCustomerAccount: function getCustomerAccount() {
-      return (0, _nodeFetch2.default)(signInData._links.account.href, {
-        headers: _extends({}, REQUEST_HEADERS_SAUCE, {
-          Authorization: 'Bearer ' + signInData.access_token
-        })
-      }).then(function (res) {
-        return res.json();
-      });
-    },
-
-    getWholeFeed: function getWholeFeed() {
-      return (0, _nodeFetch2.default)(signInData._links.events.href, {
-        headers: _extends({}, REQUEST_HEADERS_SAUCE, {
-          Authorization: 'Bearer ' + signInData.access_token
-        })
-      }).then(function (res) {
-        return res.json();
-      });
-    },
-
-    get signInData() {
-      return signInData;
-    }
-  }, (_applyDecoratedDescriptor(_obj, 'getCustomer', [withSignedInUser], (_init = Object.getOwnPropertyDescriptor(_obj, 'getCustomer'), _init = _init ? _init.value : undefined, {
-    enumerable: true,
-    configurable: true,
-    writable: true,
-    initializer: function initializer() {
-      return _init;
-    }
-  }), _obj), _applyDecoratedDescriptor(_obj, 'getCustomerAccount', [withSignedInUser], (_init2 = Object.getOwnPropertyDescriptor(_obj, 'getCustomerAccount'), _init2 = _init2 ? _init2.value : undefined, {
-    enumerable: true,
-    configurable: true,
-    writable: true,
-    initializer: function initializer() {
-      return _init2;
-    }
-  }), _obj), _applyDecoratedDescriptor(_obj, 'getWholeFeed', [withSignedInUser], (_init3 = Object.getOwnPropertyDescriptor(_obj, 'getWholeFeed'), _init3 = _init3 ? _init3.value : undefined, {
-    enumerable: true,
-    configurable: true,
-    writable: true,
-    initializer: function initializer() {
-      return _init3;
-    }
-  }), _obj)), _obj;
-};
-
-var _nodeFetch = require('node-fetch');
-
-var _nodeFetch2 = _interopRequireDefault(_nodeFetch);
+exports.default = pdfToHTML;
 
 var _lodash = require('lodash');
 
-var _api_uris = require('./api_uris');
-
-var _api_uris2 = _interopRequireDefault(_api_uris);
+var _lodash2 = _interopRequireDefault(_lodash);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) {
-  var desc = {};
-  Object['ke' + 'ys'](descriptor).forEach(function (key) {
-    desc[key] = descriptor[key];
-  });
-  desc.enumerable = !!desc.enumerable;
-  desc.configurable = !!desc.configurable;
+function pdfToHTML(_ref) {
+  var pdfDocument = _ref.pdfDocument;
 
-  if ('value' in desc || desc.initializer) {
-    desc.writable = true;
-  }
+  var children = pdfDocument.map(pdfStructureToHTMLComponent);
 
-  desc = decorators.slice().reverse().reduce(function (desc, decorator) {
-    return decorator(target, property, desc) || desc;
-  }, desc);
-
-  if (context && desc.initializer !== void 0) {
-    desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
-    desc.initializer = undefined;
-  }
-
-  if (desc.initializer === void 0) {
-    Object['define' + 'Property'](target, property, desc);
-    desc = null;
-  }
-
-  return desc;
+  return Div({}, children);
 }
 
-var REQUEST_HEADERS_SAUCE = {
-  'Content-Type': 'application/json',
-  'X-Correlation-Id': 'WEB-APP.jO4x1',
-  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-  'Origin': 'https://conta.nubank.com.br',
-  'Referer': 'https://conta.nubank.com.br/'
-};
+function Div(props, children) {
+  return createElement('div', props, children);
+}
+
+function Paragraph(props, children) {
+  return createElement('p', pdfPropsToHTMLAttrs(props), children);
+}
+
+function Span(props, children) {
+  return createElement('span', pdfPropsToHTMLAttrs(props), children);
+}
+
+function HtmlGrid(props, children) {
+  var gridSize = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
+
+  return Div({
+    class: 'ui ' + (props.size ? gridSize[props.size - 1] + ' columns' : '') + ' grid'
+  }, children);
+}
+
+function pdfStructureToHTMLComponent(component) {
+  if (_lodash2.default.isArray(component)) {
+    return Div({}, pdfStructureToHTMLComponent(component));
+  }
+
+  if (_lodash2.default.isString(component)) {
+    return Paragraph({}, [component]);
+  }
+
+  if (_lodash2.default.isString(component.text)) {
+    return Paragraph(component, [component.text]);
+  }
+
+  if (_lodash2.default.isArray(component.text)) {
+    return Paragraph(component, component.text.map(function (text) {
+      return Span(text, [text.text || text]);
+    }));
+  }
+
+  if (component.table) {
+    var children = component.table.body.map(function (row) {
+      return Div({
+        class: 'row'
+      }, row.map(function (column) {
+        return Div({
+          class: 'column'
+        }, [pdfStructureToHTMLComponent(column)]);
+      }));
+    });
+
+    return HtmlGrid({ size: component.table.body[0].length }, children);
+  }
+
+  if (component.stack) {
+    return Div({
+      class: 'ui sixteen column wide'
+    }, component.stack.map(function (segment) {
+      return Div({
+        class: 'ui sixteen column wide'
+      }, [pdfStructureToHTMLComponent(segment)]);
+    }));
+  }
+}
+
+function createElement(tagName, htmlAttrs, children) {
+  var tag = document.createElement(tagName);
+
+  // Append children
+  children.forEach(function (child) {
+    if (_lodash2.default.isString(child)) {
+      tag.innerHTML += child.replace(/\n/g, '<br/>');
+    } else {
+      tag.appendChild(child);
+    }
+  });
+
+  // Append tags
+  Object.keys(htmlAttrs).forEach(function (attr) {
+    return tag.setAttribute(attr, htmlAttrs[attr]);
+  });
+
+  return tag;
+}
+
+function pdfPropsToHTMLAttrs(pdfProps) {
+  return Object.keys(pdfProps).reduce(function (acc, prop) {
+    if (prop === 'bold') {
+      return _extends({}, acc, { style: 'font-weight: bold;' + acc.style });
+    }
+    return acc;
+  }, { style: '' });
+}
